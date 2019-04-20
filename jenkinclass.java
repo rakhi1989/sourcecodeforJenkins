f8033b5568d15b4cc18c3c66a698dd56e8c746f3
rakhi1989 <45225805+rakhi1989@users.noreply.github.com>
2019-04-20 19:06:51 +0530

    Update jenkinclass.java

--- jenkinclass.java
+++ jenkinclass.java
@@ -3,7 +3,7 @@ public class jenkinclass {
 		System.out.println("Hello World");
 		System.out.println("hooked");
 		for(int i = 0 ;i<=10;i++){
-			System.out.println("Hello   World...."+i);
+			System.out.println("Hello World...."+i);
 		}
 	}
 
